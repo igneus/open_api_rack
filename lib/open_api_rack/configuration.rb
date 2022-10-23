ca8ad29c423f54c077a472df1adff92c09c1ca70
Olga Leonteva <chhlga@jelonek-macbook.local>
2022-10-23 13:19:28 +0200

    fix

--- lib/open_api_rack/configuration.rb
+++ lib/open_api_rack/configuration.rb
@@ -1,6 +1,6 @@
 module OpenApiRack
   class Configuration
-    attr_accessor :headers_list
+    attr_accessor :headers_list, :disable_by_default
 
     def initialize(headers_list = nil, disable_by_default = false)
       @headers_list = headers_list
--- lib/open_api_rack/middleware.rb
+++ lib/open_api_rack/middleware.rb
@@ -9,7 +9,7 @@ module OpenApiRack
     def call(env)
       app_call_result = @app.call(env)
 
-      return app_call_result if skip?
+      return app_call_result if skip?(env)
 
       if File.exist?("public/open-api.yaml")
         open_api_hash = YAML.load_file("public/open-api.yaml")
@@ -142,14 +142,14 @@ module OpenApiRack
       result
     end
 
-    def skip?
+    def skip?(env)
       disable_by_default = OpenApiRack.configuration.disable_by_default
-      skip_example? = env["OA_SKIP_EXAMPLE"] == "true"
-      include_example? = env["OA_INCLUDE_EXAMPLE"] = "true"
+      skip_example = env["OA_SKIP_EXAMPLE"] == "true"
+      include_example = env["OA_INCLUDE_EXAMPLE"] == "true"
 
-      return false if include_example?
+      return false if include_example
       return true if disable_by_default
-      return true if skip_example?
+      return true if skip_example
     end
   end
 end
