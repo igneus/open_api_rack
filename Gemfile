6bf9d0abb481d6a6ea3b6242e4a870a2c49b6b66
Olga Leonteva <chhlga@jelonek-macbook.local>
2022-10-23 10:39:21 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in open_api_rack.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/open_api_rack.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_api_rack/version"
+
+module OpenApiRack
+  class Error < StandardError; end
+  # Your code goes here...
+end
